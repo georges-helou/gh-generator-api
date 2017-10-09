df865d84cf514a3d39edf5a030d51b0ae3e574e3
Georges El-Helou <georges.y.helou@gmail.com>
2017-10-09 11:37:47 +0300

    update controller

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -12,6 +12,36 @@ namespace api.Controllers
     [Route("[action]")]
     public class GenerateController : Controller
     {
+       public string MAIL_HOST;
+        public int MAIL_PORT;
+
+        public GenerateController(IOptions<MailServerConfig> mailServerConfigAccessor)
+        {
+            var config = mailServerConfigAccessor.Value;
+            MAIL_HOST = config.Host;
+            MAIL_PORT = config.Port;
+        }
+
+        [HttpPost]
+        public async Task EmailRandomNames(Range range, string email = "test@fake.com")
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = "Here are your random names!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = string.Join(Environment.NewLine, range.Of(Name.FullName))
+            };
+            using (var mailClient = new SmtpClient())
+            {
+                await mailClient.ConnectAsync(MAIL_HOST, MAIL_PORT, SecureSocketOptions.None);
+                await mailClient.SendAsync(message);
+                await mailClient.DisconnectAsync(true);
+            }
+        }
+        
         [HttpGet]
         public IEnumerable<string> Names(Range range)
             => range.Of(Name.FullName);
